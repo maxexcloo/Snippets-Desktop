00e37ec5307198b8cb1723f1a7cfe276811a15bf
Max Schaefer <max@excloo.com>
2014-08-07 22:11:12 +1000

    General Commit

---
+++ system-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Update system packages with package manager.
+
+sudo echo ">> Update Package List"
+sudo pacman -Syy
+
+echo ">> Upgrade Packages"
+sudo yaourt -Sauu
+
+echo ">> Remove Unused Packages"
+sudo pacman -Rcns $(pacman -Qdqt)
+
+echo ">> Clean Package Cache"
+sudo pacman -Sc
