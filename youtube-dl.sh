ecd9f836b5d9dc4d2bb84d324066f4e459322bd5
Max Schaefer <max@excloo.com>
2014-08-07 22:12:43 +1000

    General Commit

---
+++ youtube-dl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cookies_dir="$(mktemp -d /tmp/youtube-dl_mpv.XXXX)"
+cookies_file="${cookies_dir}/cookies"
+user_agent="$(youtube-dl --dump-user-agent)"
+video_url="$(youtube-dl --cookies="$cookies_file" --get-url --user-agent="$user_agent" "$1")"
+shift
+mpv --cookies --cookies-file="$cookies_file" --user-agent="$user_agent" "$@" -- $video_url
+rm -rf "$cookies_dir"
