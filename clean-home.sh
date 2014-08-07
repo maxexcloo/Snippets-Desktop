e7f8ca753fb6b560dbc11816d8fafb371e8fce91
Max Schaefer <max@excloo.com>
2014-08-07 21:58:46 +1000

    General Commit

--- clean-home.sh
+++ clean-home.sh
@@ -5,157 +5,122 @@
 ## Base ##
 ##########
 
-# General
-rm -rf ~/.cache
-rm -rf ~/.config/autostart
-rm -rf ~/.thumbnails
-rm -rf ~/.xsession-errors
+# Android SDK
+rm -rf ~/.android/cache
 
-# Local
-rm -rf ~/.local/share/.Tras*
-rm -rf ~/.local/share/gegl*
-rm -rf ~/.local/share/gvfs-metadata
-rm -rf ~/.local/share/icons
-rm -rf ~/.local/share/mime
-rm -rf ~/.local/share/recent*
-rm -rf ~/.local/share/Tras*
-rm -rf ~/.local/share/user-places.xbel.*
-rm -rf ~/.local/share/webkit
+# Android Studio
+rm -rf ~/.AndroidStudioBeta/system/log
+rm -rf ~/.AndroidStudioBeta/system/tmp
 
-#############
-## Desktop ##
-#############
-
-# Audacity
-rm -rf ~/.audacity-data/AutoSave
-rm -rf ~/.audacity-data/presets
-
-# Deluge
-rm -rf ~/.config/deluge/*.state
-rm -rf ~/.config/deluge/*~
-rm -rf ~/.config/deluge/icons
-rm -rf ~/.config/deluge/ipc
-
-# Dia
-rm -rf ~/.dia/*/
-rm -rf ~/.dia/menurc
-rm -rf ~/.dia/pluginrc
-
-# FileZilla
-rm -rf ~/.filezilla/bookmarks.xml
-rm -rf ~/.filezilla/filters.xml
-rm -rf ~/.filezilla/layout.xml
-rm -rf ~/.filezilla/lockfile
-rm -rf ~/.filezilla/queue.sqlite3
-rm -rf ~/.filezilla/search.xml
-rm -rf ~/.putty/randomseed
+# Firefox
+rm -rf ~/.mozilla/extensions
 
 # Flash
 rm -rf ~/.adobe
 rm -rf ~/.macromedia
 
-# GIMP
-rm -rf ~/.gimp*/*~
-rm -rf ~/.gimp*/tmp
-
-# Google Chrome
-rm -rf ~/.config/google-chrome/Default/Pepper\ Data
-rm -rf ~/.config/google-chrome/PepperFlash
-
-# GNOME
-rm -rf ~/.config/gconf
-rm -rf ~/.gconf
-rm -rf ~/.gconfd
-rm -rf ~/.gnome
-
-# Inkscape
-rm -rf ~/.config/inkscape/*.log
+# Genymotion
+rm -rf ~/.Genymobile/stats.*
 
 # IntelliJ IDEA
-rm -rf ~/.IdeaIC12/system/log/*
+rm -rf ~/.IdeaIC13/system/log
+rm -rf ~/.IdeaIC13/system/tmp
 
 # Java
 rm -rf ~/.java
 
-# JDownloader
-rm -rf ~/.jdownloader/.junique
-rm -rf ~/.jdownloader/*.log
-rm -rf ~/.jdownloader/backup
-rm -rf ~/.jdownloader/captchas/*
-rm -rf ~/.jdownloader/updateLog.txt
-
-# KDE
-rm -rf ~/.kde
-rm -rf ~/.kde4/share/appl*
-rm -rf ~/.kde4/share/autostart
-rm -rf ~/.kde4/share/kde4
-rm -rf ~/.kde4/share/wallpapers
-rm -rf ~/.kde4/shutdown
-rm -rf ~/.kde4/tmp-*/*
-
-# KDE Applications
-rm -rf ~/.kde4/share/apps/dolphin/view_properties
-rm -rf ~/.kde4/share/apps/gwenview/recent*
-rm -rf ~/.kde4/share/apps/k3b
-rm -rf ~/.kde4/share/apps/kate/metainfos
-rm -rf ~/.kde4/share/apps/kate/sessions
-rm -rf ~/.kde4/share/apps/kcookiejar
-rm -rf ~/.kde4/share/apps/kfileplaces/bookmarks.xml.*
-rm -rf ~/.kde4/share/apps/klipper
-rm -rf ~/.kde4/share/apps/konqueror
-rm -rf ~/.kde4/share/apps/korganizer/*~
-rm -rf ~/.kde4/share/apps/kpdf
-rm -rf ~/.kde4/share/apps/okular/docdata
-rm -rf ~/.kde4/share/apps/okular/structures
-rm -rf ~/.kde4/share/apps/plasma
-rm -rf ~/.kde4/share/apps/RecentDocuments
-rm -rf ~/.kde4/share/config/session
-
-# KDE History
-for i in ~/.kde4/share/config/*; do
-	sed -i 's/Replacement Text History\=.*/Replacement Text History\=/g' $i
-	sed -i 's/Search Pattern History\=.*/Search Pattern History\=/g' $i
-done
-
-# MComix
-rm -rf ~/.local/share/mcomix
-
-# Meld
-rm -rf ~/.local/share/meld
-
-# MPlayer
-rm -rf ~/.mplayer
-
-# Processing
-rm -rf ~/.processing/console/*
-rm -rf ~/.processing/debug/*
-rm -rf ~/.processing/recent.txt
-
-# RDesktops
-rm -rf ~/.rdesktop
-
-# Sublime Text
-rm -rf ~/.config/sublime-text-2/Backup
+# PuTTY
+rm -rf ~/.putty/randomseed
 
 # TeamSpeak
 rm -rf ~/.ts3client/cache
-rm -rf ~/.ts3client/logs/*
+rm -rf ~/.ts3client/logs
 
-# Transmission
-rm -rf ~/.config/transmission/*.dat
-rm -rf ~/.config/transmission/stats.json
+# Thumbnails
+rm -rf ~/.thumbnails
 
 # VirtualBox
 rm -rf ~/.VirtualBox/*.dat
 rm -rf ~/.VirtualBox/*.log
 rm -rf ~/.VirtualBox/*.log.*
 rm -rf ~/.VirtualBox/*.xml-prev
-rm -rf ~/Resources/Virtual\ Machines/*/*.vbox-prev
-rm -rf ~/Resources/Virtual\ Machines/*/Logs/*
+rm -rf ~/Virtual\ Machines/*/*.vbox-prev
+rm -rf ~/Virtual\ Machines/*/Logs
+
+# X11
+rm -rf ~/.xsession-errors
+
+############
+## Config ##
+############
+
+# Clementine
+rm -rf ~/.config/Clementine/*.bak
+rm -rf ~/.config/Clementine/jamendo.db
+rm -rf ~/.config/Clementine/spotify*
+
+# FileZilla
+rm -rf ~/.config/filezilla/queue.sqlite3
+rm -rf ~/.config/filezilla/search.xml
+
+# Inkscape
+rm -rf ~/.config/inkscape/*.log
+
+# Sublime Text
+rm -rf ~/.config/sublime-text-2/Backup
+
+# Transmission
+rm -rf ~/.config/transmission/*.dat
+
+# Unity3D
+rm -rf ~/.config/unity3d/Squad/*/*.log
+
+#########
+## KDE ##
+#########
+
+# Applications
+rm -rf ~/.kde4/share/apps/akonadi_migration_agent
+rm -rf ~/.kde4/share/apps/dolphin/view_properties
+rm -rf ~/.kde4/share/apps/kconf_update
+rm -rf ~/.kde4/share/apps/kfileplaces/bookmarks.xml.*
+rm -rf ~/.kde4/share/apps/klipper
+rm -rf ~/.kde4/share/apps/konsole/*.xml.*
+rm -rf ~/.kde4/share/apps/kopete
+rm -rf ~/.kde4/share/apps/korganizer/*~
+rm -rf ~/.kde4/share/apps/kres-migrator
+rm -rf ~/.kde4/share/apps/okular/docdata
+rm -rf ~/.kde4/share/apps/okular/structures
+rm -rf ~/.kde4/share/apps/RecentDocuments
+
+# Directories
+rm -rf ~/.kde4/share/appl*
+rm -rf ~/.kde4/share/kde4
+
+###########
+## Local ##
+###########
+
+# Akondai
+rm -rf ~/.local/share/akonadi/*.old
+rm -rf ~/.local/share/akonadi/db_data/*.old
+
+# MComix
+rm -rf ~/.local/share/mcomix
+
+# medit
+rm -rf ~/.local/share/medit-1/*~
+
+# System
+rm -rf ~/.local/share/.Tras*
+rm -rf ~/.local/share/icons
+rm -rf ~/.local/share/mime
+rm -rf ~/.local/share/recent*
+rm -rf ~/.local/share/Tras*
+rm -rf ~/.local/share/user-places.xbel.*
+
+# Unreal Engine
+rm -rf ~/.local/share/*/*/UDKGame/GLProgramBinaryCache
 
 # VLC
 rm -rf ~/.local/share/vlc
-
-# X-Chat
-rm -rf ~/.xchat2/scrollback/*/*_.txt
-rm -rf ~/.xchat2/scrollback/*/\**.txt
--- clean-system.sh
+++ clean-system.sh
@@ -4,11 +4,23 @@
 echo ">> Setting Up"
 shopt -s dotglob
 
-echo ">> Cleaning Directories"
+echo ">> Updating Caches"
+fc-cache -fr
+update-ca-certificates
+update-desktop-database
+updatedb
+
+echo ">> Cleaning Cache Directories"
 find / -type d -name '*.unison.tmp*' -exec rm -rf {} \;
 find / -type d -name '.Tras*' -exec rm -rf {} \;
 find / -type d -name 'lost+found' -exec rm -rf {} \;
-rm -rf /home/*/.cache /home/*/.local/share/Tras*
+rm -rf /home/*/.local/share/.Tras* /home/*/.local/share/Tras*
+
+echo ">> Cleaning Cache Files"
+find / -type f -name '.directory' -exec rm {} \;
+find / -type f -name '.DS_Store' -exec rm {} \;
+find / -type f -name 'desktop.ini' -exec rm {} \;
+find / -type f -name 'Thumbs.db' -exec rm {} \;
 
 echo ">> Cleaning Etc"
 find /etc/ -type f -name '*.conf.default' -exec rm {} \;
@@ -20,16 +32,4 @@ rm -rf /etc/*- /etc/iptables/* /etc/skel/*
 
 echo ">> Cleaning Pacman"
 find /etc/ -type f -name '*.pac*' -exec rm {} \;
-pacman -Sccq --noconfirm
-
-echo ">> Cleaning Temporary Files"
-find / -type f -name '.directory' -exec rm {} \;
-find / -type f -name '.DS_Store' -exec rm {} \;
-find / -type f -name 'desktop.ini' -exec rm {} \;
-find / -type f -name 'Thumbs.db' -exec rm {} \;
-
-echo ">> Updating Caches"
-fc-cache -fr
-update-ca-certificates
-update-desktop-database
-updatedb
+pacman -Scq --noconfirm
